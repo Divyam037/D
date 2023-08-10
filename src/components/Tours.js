25eea563bcfafd20680f13a58eb6a3af81f3fe73
Divyam037 <82435418+Divyam037@users.noreply.github.com>
2023-08-10 19:05:35 +0000

    heading change

--- src/components/Tours.js
+++ src/components/Tours.js
@@ -3,7 +3,7 @@ function Tours({tours, removeTour}) {
     return (
         <div className='container'>
             <div>
-                <h2 className='title'> Plan With Divyam</h2>
+                <h2 className='title'> Plan With Tushar</h2>
             </div>
             <div className='cards'>
                 {
